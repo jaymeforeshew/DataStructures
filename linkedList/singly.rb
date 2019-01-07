45da1e772b75ed6a59e5b0fe3695f459ffe6a42b
Jayme Foreshew <jrforeshew@gmail.com>
2019-01-07 18:04:25 -0500

    Adding singly linked list

---
+++ linkedList/singly.rb
@@ -0,0 +1,88 @@
+# Node class, to be elements of a linked list
+class Node
+  attr_reader :val, :next
+  attr_writer :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
+
+# LinkedList class which containing ptr to head and length
+class LinkedList
+  attr_reader :length
+
+  def initialize(node)
+    @length = 1
+    @head = node
+  end
+
+  def exists?(val)
+    tmp = @head
+    until tmp.nil?
+      return true if tmp.val == val
+
+      tmp = tmp.next
+    end
+
+    return false
+  end
+
+  def insert(node)
+    tmp = @head
+    tmp = tmp.next until tmp.next.nil?
+
+    tmp.next = node
+    @length += 1
+  end
+
+  def remove(val)
+    if @head.val == val
+      @head = @head.next
+      return
+    end
+
+    tmp = @head
+    while !tmp.nil? && !tmp.next.nil?
+      if tmp.next.val != val
+        tmp = tmp.next
+        next
+      end
+
+      tmp.next = tmp.next.next
+      @length -= 1
+      return
+    end
+  end
+
+  def print
+    list = ""
+    tmp = @head
+    until tmp.next.nil?
+      list += "#{tmp.val}->"
+      tmp = tmp.next
+    end
+
+    list += tmp.val.to_s
+    puts list
+  end
+end
+
+## Testing this out...
+head = Node.new(0)
+linked_list = LinkedList.new(head)
+
+(1..10).each do |val|
+  node = Node.new(val)
+  linked_list.insert(node)
+end
+
+linked_list.print
+puts "Exists(1) should return true: #{linked_list.exists?(1)}"
+puts "Length should return 11: #{linked_list.length}"
+puts 'Remove(4)'
+linked_list.remove(4)
+puts "Exists 4 should return false: #{linked_list.exists?(4)}"
+puts "Length should now be 10: #{linked_list.length}"
+linked_list.print
